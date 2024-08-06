8594bf92d5f53559367efa8473c9c2636be1875f
Avvrk <jomaldonado480@gmail.com>
2024-08-05 21:12:11 -0500

    d

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import { createPinia } from 'pinia';
+import PiniaPersistedState from 'pinia-plugin-persistedstate';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +15,10 @@ export default defineConfig({
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: 'src/quasar-variables.sass'
-    })
+    }),
+    
+    // Configura Pinia con el plugin de persistencia
+    createPinia().use(PiniaPersistedState),
   ]
-})
+});
+
